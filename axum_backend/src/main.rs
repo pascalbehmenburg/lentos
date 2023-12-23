027210bef9eae3df2cf2584ad37e1f0b9b95d6ea
Pascal <pascalbehmenburg@posteo.net>
2023-12-23 21:45:01 +0100

    Add error handling
    This commit should sophisticate the use of error in this package. The approach is using two primary error variants one for the display to external users and one for internal logs. Also there is a fallback variant if none apply for example when converting another error into an internal one without an addition message.

--- axum_backend/src/main.rs
+++ axum_backend/src/main.rs
@@ -1,8 +1,6 @@
-use std::borrow::Cow;
-use std::{
-    fs::File, io::BufReader, panic::panic_any, path::PathBuf, sync::Arc,
-};
+use std::{fs::File, io::BufReader, panic::panic_any, path::PathBuf, sync::Arc};
 
+use app_dirs2::{app_root, AppDataType, AppInfo};
 use async_trait::async_trait;
 use axum::{
     extract::{FromRequest, Host, Request},
@@ -24,19 +22,42 @@ use tokio_rustls::{
 use tower_http::compression::CompressionLayer;
 use tower_service::Service;
 
-#[derive(Clone, Copy)]
-struct Ports {
-    http: u16,
-    https: u16,
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+struct BackendConfig {
+    pub ip_address: String,
+    pub http_port: u16,
+    pub https_port: u16,
+    pub cert_file_path: PathBuf,
+    pub key_file_path: PathBuf,
+    pub database_url: String,
+    pub signing_key: String,
+}
+
+fn load_config() -> Result<BackendConfig> {
+    const APP_INFO: AppInfo = AppInfo { name: "lentos_backend", author: "lentos" };
+
+    let config_path = app_root(AppDataType::UserConfig, &APP_INFO)
+        .map_err(|e| internal_error!("User config directory not found. Details: {}", e))?
+        .join("Config.toml");
+
+    if !config_path.exists() {
+        return Err(internal_error!(
+            "Backend config not found. Details: {}",
+            config_path.display()
+        ));
+    }
+
+    toml::from_str::<BackendConfig>(&std::fs::read_to_string(config_path).map_err(|e| {
+        internal_error!("Backend config has bad encoding. Ensure it is UTF-8. Details: {}", e)
+    })?)
+    .map_err(|err| internal_error!("Failed to parse backend config. Details: {}", err))
 }
 
 fn router() -> Router {
     Router::new()
         .layer(
             // enforce brotli compression on all responses
-            CompressionLayer::new()
-                .br(true)
-                .compress_when(|_, _, _: &_, _: &_| true),
+            CompressionLayer::new().br(true).compress_when(|_, _, _: &_, _: &_| true),
         )
         .layer(tower_http::trace::TraceLayer::new_for_http())
 }
@@ -47,7 +68,7 @@ fn add_routes(router: Router) -> Router {
     }
 
     async fn handler_404() -> impl IntoResponse {
-        (StatusCode::NOT_FOUND, "Error 404: Not found")
+        response_error!(StatusCode::NOT_FOUND, "Error 404: Not found")
     }
 
     router.route("/", get(handler)).fallback(handler_404)
@@ -55,9 +76,7 @@ fn add_routes(router: Router) -> Router {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    const IP_ADDRESS: &str = "127.0.0.1";
-
-    let ports = Ports { http: 8080, https: 8443 };
+    let config = load_config()?;
 
     // init tracing (logs) and error handling (color_eyre)
     {
@@ -77,42 +96,39 @@ async fn main() -> Result<()> {
             .with_target("tower_http", LevelFilter::DEBUG)
             .with_default(LevelFilter::DEBUG);
 
-        let subscriber = Registry::default()
-            .with(lib_filter_layer)
-            .with(fmt_layer)
-            .with(ErrorLayer::default());
+        let subscriber =
+            Registry::default().with(lib_filter_layer).with(fmt_layer).with(ErrorLayer::default());
 
         set_global_default(subscriber)
-            .wrap_err("Failed to set tracing subscriber.")?;
+            .map_err(|_| internal_error!("Failed to set global tracing subscriber"))?;
         color_eyre::install()
-            .wrap_err("Failed to install color_eyre error handler.")?;
+            .map_err(|_| internal_error!("Failed to install color_eyre error handler."))?;
         tracing::info!("Tracing setup complete.");
     }
 
     // this sets up the tls config
     let rustls_config = {
-        let cert_file_path = PathBuf::from("127.0.0.1+1.pem");
+        let cert_file_path = PathBuf::from(config.cert_file_path);
         let mut cert_reader = BufReader::new(
             File::open(cert_file_path)
-                .wrap_err("Certificate file not found.")?,
+                .map_err(|_| internal_error!("Certificate file not found."))?,
         );
 
-        let key_file_path = PathBuf::from("127.0.0.1+1-key.pem");
+        let key_file_path = PathBuf::from(config.key_file_path);
         let mut key_reader = BufReader::new(
-            File::open(key_file_path).wrap_err("Key file not found.")?,
+            File::open(key_file_path).map_err(|_| internal_error!("Key file not found."))?,
         );
 
         let key = PrivateKey(
             pkcs8_private_keys(&mut key_reader)
-                .wrap_err(
-                    "Failed to construct pkcs8 private key. Check the key \
-                     file.",
-                )?
+                .map_err(|e| {
+                    internal_error!("Failed to construct pkcs8 private key. Details: {}", e)
+                })?
                 .remove(0),
         );
 
         let certs = certs(&mut cert_reader)
-            .wrap_err("Failed to construct certs. Check the certificates.")?
+            .map_err(|e| internal_error!("Failed to construct certs. {}", e))?
             .into_iter()
             .map(Certificate)
             .collect();
@@ -121,10 +137,12 @@ async fn main() -> Result<()> {
             .with_safe_defaults()
             .with_no_client_auth()
             .with_single_cert(certs, key)
-            .wrap_err(
-                "Failed to construct tls server config. Check the cert / key \
-                 files.",
-            )?;
+            .map_err(|e| {
+                internal_error!(
+                    "Invalid private key provided when building rustls config. Details: {}",
+                    e
+                )
+            })?;
 
         config.alpn_protocols = vec![b"h2".to_vec()];
 
@@ -134,57 +152,64 @@ async fn main() -> Result<()> {
     };
 
     // http to https redirect service
+    let ip_address = config.ip_address.to_string();
     tokio::spawn(async move {
-        fn make_https(host: String, uri: Uri, ports: Ports) -> Result<Uri> {
-            let mut parts = uri.into_parts();
+        let redirect = {
+            let http_port = config.http_port.to_string();
+            let https_port = config.https_port.to_string();
 
-            parts.scheme = Some(axum::http::uri::Scheme::HTTPS);
+            move |Host(host): Host, uri: Uri| async move {
+                let make_https = |host: String, uri: Uri| -> Result<Uri> {
+                    let mut parts = uri.into_parts();
 
-            if parts.path_and_query.is_none() {
-                parts.path_and_query = Some("/".parse().unwrap());
-            }
+                    parts.scheme = Some(axum::http::uri::Scheme::HTTPS);
 
-            let https_host =
-                host.replace(&ports.http.to_string(), &ports.https.to_string());
-            parts.authority = Some(https_host.parse()?);
+                    if parts.path_and_query.is_none() {
+                        parts.path_and_query = Some("/".parse().unwrap());
+                    }
 
-            Ok(Uri::from_parts(parts)?)
-        }
+                    let https_host = host.replace(&http_port, &https_port);
 
-        let redirect = move |Host(host): Host, uri: Uri| async move {
-            match make_https(host, uri, ports) {
-                Ok(uri) => Ok(Redirect::permanent(&uri.to_string())),
-                Err(error) => {
-                    tracing::warn!(%error, "failed to convert URI to HTTPS");
-                    Err(StatusCode::BAD_REQUEST)
+                    parts.authority = Some(https_host.parse().map_err(|e| {
+                        internal_error!(
+                            "Failed replacing the http URI host with https. Details: {}",
+                            e
+                        )
+                    })?);
+
+                    Uri::from_parts(parts).map_err(|e| {
+                        internal_error!("Failed to construct URI from parts. Details: {}", e)
+                    })
+                };
+
+                match make_https(host, uri) {
+                    Ok(uri) => Ok(Redirect::permanent(&uri.to_string())),
+                    Err(error) => Err(internal_error!(
+                        "Failed to redirect from http to https. Details: {}",
+                        error
+                    )),
                 }
             }
         };
 
         let mut listenfd = ListenFd::from_env();
-        let tcp_listener = match listenfd
-            .take_tcp_listener(0)
-            .expect("fd at position 0 was not an tcp listener")
-        {
-            Some(tcp_listener) => {
-                TcpListener::from_std(tcp_listener).expect("")
-            }
-            None => TcpListener::bind(format!("{}:{}", IP_ADDRESS, ports.http))
+        let tcp_listener = match listenfd.take_tcp_listener(0).unwrap_or_else(|_| {
+            panic_any(internal_error!("fd at position 0 was not an tcp listener"))
+        }) {
+            Some(tcp_listener) => TcpListener::from_std(tcp_listener).expect(""),
+            None => TcpListener::bind(format!("{}:{}", &ip_address, &config.http_port))
                 .await
                 .unwrap_or_else(|_| {
-                    panic_any(color_eyre::eyre::eyre!(
-                        "failed to bind systemfd tcp listener to adress using \
-                         fallback: {:?}:{:?}",
-                        IP_ADDRESS,
-                        ports.http
+                    panic_any(internal_error!(
+                        "Failed to bind systemfd tcp listener to address using fallback: {:?}:{:?}",
+                        &ip_address,
+                        &config.http_port
                     ))
                 }),
         };
         tracing::info!(
             "Listening to http requests from {}",
-            tcp_listener
-                .local_addr()
-                .expect("Failed to get address of tcp listener")
+            tcp_listener.local_addr().expect("Failed to get address of tcp listener")
         );
         axum::serve(tcp_listener, redirect.into_make_service())
             .await
@@ -194,16 +219,15 @@ async fn main() -> Result<()> {
     let mut listenfd = ListenFd::from_env();
     let tcp_listener = match listenfd
         .take_tcp_listener(1)
-        .wrap_err("fd at position 1 was not an tcp listener")?
+        .unwrap_or_else(|_| panic_any(internal_error!("fd at position 1 was not an tcp listener")))
     {
         Some(tcp_listener) => TcpListener::from_std(tcp_listener).expect(""),
-        None => TcpListener::bind(format!("{}:{}", IP_ADDRESS, ports.https))
+        None => TcpListener::bind(format!("{}:{}", &config.ip_address, &config.https_port))
             .await
             .unwrap_or_else(|_| {
                 panic_any(color_eyre::eyre::eyre!(
-                    "failed to bind systemfd tcp listener to adress using \
-                     fallback: {:?}",
-                    IP_ADDRESS
+                    "Failed to bind systemfd tcp listener to adress using fallback: {:?}",
+                    config.ip_address
                 ))
             }),
     };
@@ -219,10 +243,9 @@ async fn main() -> Result<()> {
         let app_service = app_router.clone();
         let tls_acceptor = tls_acceptor.clone();
 
-        let (conn, addr) = tcp_listener
-            .accept()
-            .await
-            .wrap_err("Failed to accept tcp connection.")?;
+        let (conn, addr) = tcp_listener.accept().await.map_err(|e| {
+            internal_error!("Failed to accept incoming tcp connection. Details: {}", e)
+        })?;
 
         tokio::spawn(async move {
             // wait for tls handshake to happen
@@ -234,11 +257,9 @@ async fn main() -> Result<()> {
             // convert the tokio stream to a hyper stream and forward the
             // incoming requests to our axum app
             let stream = TokioIo::new(stream);
-            let hyper_service = hyper::service::service_fn(
-                move |request: Request<Incoming>| {
-                    app_service.clone().call(request)
-                },
-            );
+            let hyper_service = hyper::service::service_fn(move |request: Request<Incoming>| {
+                app_service.clone().call(request)
+            });
 
             hyper_util::server::conn::auto::Builder::new(TokioExecutor::new())
                 .serve_connection_with_upgrades(stream, hyper_service)
@@ -260,26 +281,18 @@ where
 {
     type Rejection = Response;
 
-    async fn from_request(
-        req: Request,
-        _state: &S,
-    ) -> Result<Self, Self::Rejection> {
+    async fn from_request(req: Request, _state: &S) -> Result<Self, Self::Rejection> {
         let content_type_header = req.headers().get(CONTENT_TYPE);
-        let content_type =
-            content_type_header.and_then(|value| value.to_str().ok());
+        let content_type = content_type_header.and_then(|value| value.to_str().ok());
 
         if let Some(content_type) = content_type {
             if content_type.starts_with(mime::APPLICATION_JSON.as_ref()) {
-                let Json(payload) =
-                    req.extract().await.map_err(IntoResponse::into_response)?;
+                let Json(payload) = req.extract().await.map_err(IntoResponse::into_response)?;
                 return Ok(Self(payload));
             }
 
-            if content_type
-                .starts_with(mime::APPLICATION_WWW_FORM_URLENCODED.as_ref())
-            {
-                let Form(payload) =
-                    req.extract().await.map_err(IntoResponse::into_response)?;
+            if content_type.starts_with(mime::APPLICATION_WWW_FORM_URLENCODED.as_ref()) {
+                let Form(payload) = req.extract().await.map_err(IntoResponse::into_response)?;
                 return Ok(Self(payload));
             }
         }
@@ -292,10 +305,10 @@ pub type Result<T, E = Error> = std::result::Result<T, E>;
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("{0}, {1}")]
-    ResponseError(StatusCode, &'static str),
+    ResponseError(StatusCode, String),
 
     #[error("{0}")]
-    InternalError(&'static str),
+    InternalError(String),
 
     // fallback error which is treated like an internal error
     #[error(transparent)]
@@ -303,15 +316,15 @@ pub enum Error {
 }
 
 #[macro_export]
-macro_rules! custom_error {
+macro_rules! internal_error {
     ($msg:literal $(,)?) => {
-        Err(Error::InternalError(format!($msg)))
+        Error::InternalError(format!($msg))
     };
     ($err:expr $(,)?) => {
-        Err(Error::from($err))
+        Error::from($err)
     };
     ($fmt:expr, $($arg:tt)*) => {
-        Err(Error::InternalError(format!($fmt, $($arg)*)))
+        Error::InternalError(format!($fmt, $($arg)*))
     };
 }
 
@@ -319,29 +332,37 @@ macro_rules! custom_error {
 #[macro_export]
 macro_rules! response_error {
     ($status:expr, $msg:literal $(,)?) => {
-        Err(Error::ResponseError($status, format!($msg)))
+        Error::ResponseError($status, format!($msg))
     };
     ($status:expr, $fmt:expr, $($arg:tt)*) => {
-        Err(Error::ResponseError($status, format!($fmt, $($arg)*)))
+        Error::ResponseError($status, format!($fmt, $($arg)*))
     };
 }
 
 impl IntoResponse for Error {
     fn into_response(self) -> Response {
+        #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+        struct ErrorMessage {
+            error: String,
+        }
+
         match self {
             Error::ResponseError(status_code, error_message) => {
-                // Expected user caused errors are logged on info level
-                tracing::info!("{}{}", status_code, error_message);
-                (status_code, error_message.to_string()).into_response()
+                let json = serde_json::to_value(ErrorMessage { error: error_message }).unwrap();
+
+                tracing::info!("Error: {}", json);
+
+                (status_code, Json::<serde_json::Value>(json)).into_response()
             }
             _ => {
-                // Unexpected errors are logged on error level
-                tracing::error!("{:?}", self);
-                (
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                    "Error 500: Something went wrong! We're working on it.",
-                )
-                    .into_response()
+                let json = serde_json::to_value(ErrorMessage {
+                    error: "Something went wrong! We're working on it.".into(),
+                })
+                .unwrap();
+
+                tracing::error!("Error: {}", self);
+
+                (StatusCode::INTERNAL_SERVER_ERROR, Json::<serde_json::Value>(json)).into_response()
             }
         }
     }
@@ -374,10 +395,7 @@ mod tests {
             .uri("/test")
             .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
             .body(Body::from(
-                serde_json::to_string(&Payload {
-                    name: "MemeJson".to_string(),
-                })
-                .unwrap(),
+                serde_json::to_string(&Payload { name: "MemeJson".to_string() }).unwrap(),
             ))
             .unwrap();
 
@@ -389,15 +407,9 @@ mod tests {
         let request = Request::builder()
             .method(http::Method::POST)
             .uri("/test")
-            .header(
-                http::header::CONTENT_TYPE,
-                mime::APPLICATION_WWW_FORM_URLENCODED.as_ref(),
-            )
+            .header(http::header::CONTENT_TYPE, mime::APPLICATION_WWW_FORM_URLENCODED.as_ref())
             .body(Body::from(
-                serde_urlencoded::to_string(Payload {
-                    name: "MemeForm".to_string(),
-                })
-                .unwrap(),
+                serde_urlencoded::to_string(Payload { name: "MemeForm".to_string() }).unwrap(),
             ))
             .unwrap();
 
